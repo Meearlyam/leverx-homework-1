b556eccd5bf683b6fece8a09e3cbe8e8e8d4d0a7
Meearlyam <shavela99@gmail.com>
2021-03-31 22:36:36 +0300

    Edit HelloLeverX - it uses Apache Commons to refer to your name and surname in greeting LeverX

--- README.md
+++ README.md
@@ -1,31 +1,25 @@
 ## Hello, LeverX app
 
-* To compile this app use the following command:
-~~~
-javac src/by/meearlyam/helloleverx/HelloLeverX.java
-~~~
-* To run the app use
-~~~
-java -cp ./src by.meearlyam.helloleverx.HelloLeverX
-~~~
 * src directory is supposed to store packages with java source files, so to separate source code and binary files (with .class extension) you need to create bin directory
 (mkdir bin) and run the following command:
 ~~~
-javac -d bin src/by/meearlyam/helloleverx/HelloLeverX.java
+javac -d bin -cp lib/commons-text-1.9.jar src/by/meearlyam/helloleverx/HelloLeverX.java
 ~~~
 * To run then use
 ~~~
-java -cp ./bin by.meearlyam.helloleverx.HelloLeverX
+java -cp ./bin;lib/commons-text-1.9.jar;lib/commons-lang3-3.12.0.jar by.meearlyam.helloleverx.HelloLeverX
 ~~~
 ### If you want to create a jar
 
-* First, create MANIFEST.MF to point java to main class:
+* First, fill MANIFEST.MF to point java to main class and all dependencies. It is IMPORTANT that this file ends with empty line:
 ~~~
-echo Main-Class: by.meearlyam.helloleverx.HelloLeverX > MANIFEST.MF
+Main-Class: by.meearlyam.helloleverx.HelloLeverX
+class-path: lib/commons-text-1.9.jar lib/commons-lang3-3.12.0.jar
+
 ~~~
 * Then, to create a jar that preserves the package structure use command:
 ~~~
-jar -cfm helloleverx.jar manifest.mf -C bin .
+jar -cmf manifest.mf helloleverx.jar -C bin .
 ~~~
 * Command to run the jar:
 ~~~
--- src/by/meearlyam/helloleverx/HelloLeverX.java
+++ src/by/meearlyam/helloleverx/HelloLeverX.java
@@ -3,58 +3,61 @@ package by.meearlyam.helloleverx;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import org.apache.commons.mail.*;
+import java.util.Map;
+import java.util.HashMap;
+import org.apache.commons.text.*;
 
 /**
  * HelloLeverX class
  * 
  * @author Vera Shavela
- * @version 1.0.0
+ * @version 2.0.0
  */
 public class HelloLeverX {
 
 	/**
-	 * This method ask your name and email and then greets you by sending message "Hello, [name]!" to your email.
+	 * This method ask your name and surname and then greets LeverX on your behalf
 	 *
 	 * @param args represents the arguments given to this method when running HelloLeverX
 	 */
 	public static void main(String... args) {
-		String name;
-		String fromEmail = "shavela99@gmail.com";
-		String toEmail;
-
+		
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+			String name;
+			String surname;
 			
 			System.out.println("Enter your name:");
 			name = reader.readLine();
 
-			System.out.println("Enter your email to get the greeting:");
-			toEmail = reader.readLine();
+			System.out.println("Enter your surname:");
+			surname = reader.readLine();
 
 			if(name == null) {
 				name = "Noname";
 			}
 
-			if(toEmail == null) {
-				toEmail = fromEmail;
+			if(surname == null) {
+				surname = "Nosurname";
 			}
+
+			Map<String, String> valuesMap = new HashMap<>();
+			valuesMap.put("name", name);
+			valuesMap.put("surname", surname);
+			
+			String templateString = "Hello, LeverX, from ${name} ${surname}!";
+
+			StringSubstitutor sub = new StringSubstitutor(valuesMap);
+
+			String resolvedString = sub.replace(templateString);
+
+			System.out.println(resolvedString);
 		}
 		catch (IOException e) {
 			System.out.println(
 				String.format("IOException in main(): %s", e.getMessage())
 			);
 		}
-		
-		Email email = new SimpleEmail();
-		email.setHostName("smtp.googlemail.com");
-		email.setSmtpPort(465);
-		email.setAuthenticator(new DefaultAuthenticator("username", "password"));
-		email.setSSLOnConnect(true);
-		email.setFrom(fromEmail);
-		email.setSubject("TestMail");
-		email.setMsg(String.format("Hello, %s", name));
-		email.addTo(toEmail);
-		email.send();
-		System.out.println("Hello email was successfully sent :)");
+
 	}
 }
