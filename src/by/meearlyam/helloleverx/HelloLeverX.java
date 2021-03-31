bd99e704b2197513c3ae1db8ec75dbb1e0a0da22
Meearlyam <shavela99@gmail.com>
2021-03-31 15:46:40 +0300

    Initial commit

---
+++ src/by/meearlyam/helloleverx/HelloLeverX.java
@@ -0,0 +1,19 @@
+package by.meearlyam.helloleverx;
+
+/**
+ * HelloLeverX class
+ * 
+ * @author Vera Shavela
+ * @version 1.0.0
+ */
+public class HelloLeverX {
+
+	/**
+	 * This method just greets LeverX by printing hardcoded string "Hello, LeverX!" to standart output.
+	 *
+	 * @param args represents the arguments given to this method when running HelloLeverX
+	 */
+	public static void main(String... args) {
+		System.out.println("Hello, LeverX!");
+	}
+}
